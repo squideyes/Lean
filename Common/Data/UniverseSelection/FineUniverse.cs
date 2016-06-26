abb0ffb4ddd5b5a87e60bb77538d1908399ecca2
Stefano Raggi <stefano.raggi67@alice.it>
2016-06-26 15:07:22 +0200

    Removes duplicate FineFundamental class

--- Common/Data/UniverseSelection/FineUniverse.cs
+++ Common/Data/UniverseSelection/FineUniverse.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using QuantConnect.Data.Fundamental;
 using QuantConnect.Securities;
 
 namespace QuantConnect.Data.UniverseSelection
